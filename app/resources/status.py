5b6ec07ceeb1bd8672a6f2653a23b6c1177e6063
Raman Hrabun <rhdocld@gmail.com>
2023-10-06 13:43:19 +0200

    Added hostname to visualize scale tests

--- app/resources/status.py
+++ app/resources/status.py
@@ -1,7 +1,13 @@
+import socket
+
 from flask_restful import Resource
 
 
 class Status(Resource):
     def get(self):
+        hostname = socket.gethostname()
+        ip = socket.gethostbyname(hostname)
         return {'status': 'Api is working',
+                'hostname': f'{hostname}',
+                'ip': f'{ip}',
                 'apiVersion': '1'}, 200
